d7a90f53d445d9a80c19079692b694198f8a8f20
OtsuKotsu <otoxukk.y@gmail.com>
2022-01-20 23:16:51 +0900

    latest experiments

--- task1and2/experiments/20220120-training-2.py
+++ task1and2/experiments/20220120-training-2.py
@@ -10,29 +10,26 @@ sys.path.append(str(project_root))
 
 
 """main"""
-from typing import Dict  # noqa (E402)
-
 import matplotlib.pyplot as plt  # noqa (E402)
 import torch  # noqa (E402)
 from torch import nn  # noqa (E402)
-from torch.nn import functional as F  # noqa (E402)
 
 from corsmal_challenge.data.data_loader import (  # noqa (E402)
     ReproducibleDataLoader as DataLoader,
 )
 from corsmal_challenge.data.dataset import AudioDataset  # noqa (E402)
-from corsmal_challenge.models.task1_2 import TaskChallenger2  # noqa (E402)
+from corsmal_challenge.models.task1_2 import TaskChallenger4  # noqa (E402)
 from corsmal_challenge.train.train_val import classification_loop  # noqa (E402)
 from corsmal_challenge.utils import fix_random_seeds  # noqa (E402)
 
 RAND_SEED = 0
-EPOCH = 100
+EPOCH = 150
 
 if __name__ == "__main__":
     fix_random_seeds(RAND_SEED)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = TaskChallenger2()
+    model = TaskChallenger4()
     model = model.to(device)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), momentum=0.5, lr=0.001)
@@ -113,14 +110,14 @@ if __name__ == "__main__":
             min_val_loss_sum_t1_t2 = val_loss_t1[-1] + val_loss_t2[-1]
             best_loss_pair = val_loss_t1[-1], val_loss_t2[-1]
             best_acc_pair = val_acc_t1, val_acc_t2
-            torch.save(model.state_dict(), current_dir / "20220116-training-result.pt")
+            torch.save(model.state_dict(), current_dir / "20220120-training-2-result.pt")
 
     plt.plot(train_loss_t1, label="train loss: t1")
     plt.plot(val_loss_t1, label="val loss: t1")
     plt.plot(train_loss_t2, label="train loss: t2")
     plt.plot(val_loss_t2, label="val loss: t2")
     plt.legend()
-    plt.savefig(str(current_dir / "20220116-training-result.png"))
+    plt.savefig(str(current_dir / "20220120-training-2-result.png"))
 
     print(f"best (val_loss_t1, val_loss_t2) pair is {best_loss_pair}!")
     print(f"then (val_acc_t1, val_acc_t2) pair is {best_acc_pair}!")
@@ -128,18 +125,18 @@ if __name__ == "__main__":
 # STDOUT
 # ```
 # :
-# {'train loss': 0.045215701981289665, 'train accuracy': 0.9818181818181818, \
-#       'val loss': 0.15649232095998764, 'val accuracy': 0.9487179487179487}
-# {'train loss': 0.09526595299514952, 'train accuracy': 0.9636363636363636, \
-#       'val loss': 0.5622158379012708, 'val accuracy': 0.8076923076923077}
-# {'train loss': 0.06335140040479204, 'train accuracy': 0.9785123966942149, \
-#       'val loss': 0.3877027627267219, 'val accuracy': 0.9358974358974359}
-# {'train loss': 0.10734112852360589, 'train accuracy': 0.9504132231404959, \
-#       'val loss': 0.21184918087817112, 'val accuracy': 0.8717948717948718}
-# {'train loss': 0.025730110860609577, 'train accuracy': 0.9900826446280991, \
-#       'val loss': 0.26380806961285486, 'val accuracy': 0.9230769230769231}
-# {'train loss': 0.11475210154677912, 'train accuracy': 0.9553719008264463, \
-#       'val loss': 0.37617537109571375, 'val accuracy': 0.8589743589743589}
-# best (val_loss_t1, val_loss_t2) pair is (0.13818703214239855, 0.18227529815692087)!
-# then (val_acc_t1, val_acc_t2) pair is (0.9487179487179487, 0.9102564102564102)!
+# {'train loss': 0.03728779871972468, 'train accuracy': 0.9867768595041322, \
+#       'val loss': 0.10871001488149254, 'val accuracy': 0.9615384615384616}
+# {'train loss': 0.031024785050870442, 'train accuracy': 0.9917355371900827, \
+#       'val loss': 0.9776671341283579, 'val accuracy': 0.7564102564102564}
+# {'train loss': 0.07070674430640347, 'train accuracy': 0.9834710743801653, \
+#       'val loss': 0.14151212510526606, 'val accuracy': 0.9615384615384616}
+# {'train loss': 0.02485862942431711, 'train accuracy': 0.9917355371900827, \
+#       'val loss': 0.6063724803497845, 'val accuracy': 0.8589743589743589}
+# {'train loss': 0.03606577391804771, 'train accuracy': 0.9884297520661157, \
+#       'val loss': 0.22414980898134254, 'val accuracy': 0.9102564102564102}
+# {'train loss': 0.008726799127156477, 'train accuracy': 1.0, \
+#       'val loss': 0.5411773054698263, 'val accuracy': 0.8717948717948718}
+# best (val_loss_t1, val_loss_t2) pair is (0.08326099165004472, 0.20015583899676598)!
+# then (val_acc_t1, val_acc_t2) pair is (0.9615384615384616, 0.9102564102564102)!
 # ```
