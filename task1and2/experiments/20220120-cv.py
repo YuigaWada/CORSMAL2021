3fc425bb1cdee4e77020970414842b9be0153337
OtsuKotsu <otoxukk.y@gmail.com>
2022-01-21 15:18:25 +0900

    update cv code

--- task1and2/experiments/20220120-cv.py
+++ task1and2/experiments/20220120-cv.py
@@ -13,7 +13,7 @@ sys.path.append(str(project_root))
 """main"""
 import json  # noqa (E402)
 import random  # noqa (E402)
-from typing import Dict, List  # noqa (E402)
+from typing import Dict, List, Tuple  # noqa (E402)
 
 import matplotlib.pyplot as plt  # noqa (E402)
 import numpy as np  # noqa (E402)
@@ -29,8 +29,11 @@ from corsmal_challenge.train.train_val import classification_loop  # noqa (E402)
 from corsmal_challenge.utils import fix_random_seeds  # noqa (E402)
 
 RAND_SEED = 0
-FOLDS = 5
-EPOCH = 80
+FOLDS = 9
+# FOLD_SKIP_FREQ = 9
+FOLD_SKIP_FREQ = 3
+EPOCH = 100
+# EPOCH = 120
 
 
 if __name__ == "__main__":
@@ -53,18 +56,39 @@ if __name__ == "__main__":
         filling_level = data["filling level"]
         classified_data[filling_type][filling_level].append(id)
 
+    # for typ, d in enumerate(classified_data):  # type
+    #     for lvl, id_list in enumerate(d):  # level
+    #         random.shuffle(id_list)
+    #         np_arrays = np.array_split(id_list, FOLDS)
+    #         lists: List[List[int]] = [list(np_array) for np_array in np_arrays]
+    #         for i in range(FOLDS):
+    #             ids[i].extend(lists[i])
+    #             labels[i].extend([Labels(typ, lvl) for _ in range(len(lists[i]))])
+
+    ids_labels: List[List[Tuple[int, Labels]]] = [[] for _ in range(FOLDS)]
     for typ, d in enumerate(classified_data):  # type
         for lvl, id_list in enumerate(d):  # level
-            random.shuffle(id_list)
-            np_arrays = np.array_split(id_list, FOLDS)
-            lists: List[List[int]] = [list(np_array) for np_array in np_arrays]
-            for i in range(FOLDS):
-                ids[i].extend(lists[i])
-                labels[i].extend([Labels(typ, lvl) for _ in range(len(lists[i]))])
+            fld = 0
+            for idx in id_list:
+                ids_labels[fld].append((idx, Labels(typ, lvl)))
+                fld = (fld + 1) % FOLDS
+    for fld in range(FOLDS):
+        random.shuffle(ids_labels[fld])
+        for id, label in ids_labels[fld]:
+            ids[fld].append(id)
+            labels[fld].append(label)
 
     # cross validation
     loss_acc_summaries: List[str] = []
     for fold_id in range(FOLDS):
+        if fold_id % FOLD_SKIP_FREQ != 0:
+            print("")
+            print(f"FOLD.{fold_id + 1} => SKIP")
+            continue
+        else:
+            print("")
+            print(f"FOLD.{fold_id + 1} => START")
+
         train_idx: List[int] = []
         train_label: List[Labels] = []
         for i in list(range(FOLDS))[:fold_id] + list(range(FOLDS))[fold_id + 1 :]:
@@ -78,7 +102,7 @@ if __name__ == "__main__":
         model = model.to(device)
         loss_fn = nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(), momentum=0.5, lr=0.001)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.50)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.80)
         train_dataset = SimpleAudioDataset(train_idx, train_label, data_dir)
         val_dataset = SimpleAudioDataset(val_idx, val_label, data_dir)
         train_dataloader = DataLoader(train_dataset, specified_seed=RAND_SEED, shuffle=True)
@@ -90,36 +114,18 @@ if __name__ == "__main__":
         val_loss_t2: List[float] = []
 
         min_val_loss_sum_t1_t2: float = float("inf")
+        best_epoch_id = -1
         best_loss_pair = (12.34, 45.67)
         best_acc_pair = (0.0, 0.0)
 
+        plt.figure()
         for step in range(EPOCH):
             train_dataset.query = "type"
             val_dataset.query = "type"
             train_dataset.random_crop = True
             train_dataset.strong_crop = True
+            train_dataset.clip_end = False
             model.task_id = 2
-
-            tup = classification_loop(  # type: ignore
-                device,
-                model,
-                loss_fn,
-                optimizer,
-                train_dataloader,
-                val_dataloader,
-                enable_amp=True,
-            )
-            metrics = tup[1]
-            train_loss_t1.append(metrics["train loss"])
-            val_loss_t1.append(metrics["val loss"])
-            val_acc_t1 = metrics["val accuracy"]
-            print(metrics)
-
-            train_dataset.query = "level"
-            val_dataset.query = "level"
-            train_dataset.random_crop = False
-            train_dataset.strong_crop = False
-            model.task_id = 1
 
             tup = classification_loop(  # type: ignore
                 device,
@@ -134,7 +140,37 @@ if __name__ == "__main__":
             train_loss_t2.append(metrics["train loss"])
             val_loss_t2.append(metrics["val loss"])
             val_acc_t2 = metrics["val accuracy"]
-            print(metrics)
+            print(f"  EPOCH.{str(step).zfill(3)}  <task2> Type Classification")
+            print("               ", end="")
+            for mtrc, value in metrics.items():
+                print(f"{mtrc}: {value:.3f}", end=" | ")
+            print("")
+
+            train_dataset.query = "level"
+            val_dataset.query = "level"
+            train_dataset.random_crop = False
+            train_dataset.strong_crop = False
+            train_dataset.clip_end = True
+            model.task_id = 1
+
+            tup = classification_loop(  # type: ignore
+                device,
+                model,
+                loss_fn,
+                optimizer,
+                train_dataloader,
+                val_dataloader,
+                enable_amp=True,
+            )
+            metrics = tup[1]
+            train_loss_t1.append(metrics["train loss"])
+            val_loss_t1.append(metrics["val loss"])
+            val_acc_t1 = metrics["val accuracy"]
+            print("             <task1> Level Classification")
+            print("               ", end="")
+            for mtrc, value in metrics.items():
+                print(f"{mtrc}: {value:.3f}", end=" | ")
+            print("")
 
             lr_scheduler.step()
 
@@ -142,18 +178,21 @@ if __name__ == "__main__":
                 min_val_loss_sum_t1_t2 = val_loss_t1[-1] + val_loss_t2[-1]
                 best_loss_pair = val_loss_t1[-1], val_loss_t2[-1]
                 best_acc_pair = val_acc_t1, val_acc_t2
-                torch.save(model.state_dict(), current_dir / f"20220120-cv-result-fold{fold_id}.pt")
+                best_epoch_id = step  # 0-indexed
+                torch.save(model.state_dict(), current_dir / f"20220120-cv-result-fold{fold_id + 1}.pt")
 
         plt.plot(train_loss_t1, label="train loss: t1")
         plt.plot(val_loss_t1, label="val loss: t1")
         plt.plot(train_loss_t2, label="train loss: t2")
         plt.plot(val_loss_t2, label="val loss: t2")
         plt.legend()
-        plt.savefig(str(current_dir / f"20220120-cv-result-fold{fold_id}.png"))
+        plt.savefig(str(current_dir / f"20220120-cv-result-fold{fold_id + 1}.png"))
 
-        loss_acc_summaries.append(f"Fold.{fold_id}")
-        loss_acc_summaries.append(f"best (val_loss_t1, val_loss_t2) pair is {best_loss_pair}!")
-        loss_acc_summaries.append(f"then (val_acc_t1, val_acc_t2) pair is {best_acc_pair}!")
+        loss_acc_summaries.append(f"Fold.{fold_id + 1}")
+        loss_acc_summaries.append(f"  on epoch.{best_epoch_id}")
+        loss_acc_summaries.append(f"  best (val_loss_t1, val_loss_t2) pair is {best_loss_pair}!")
+        loss_acc_summaries.append(f"  then (val_acc_t1, val_acc_t2) pair is {best_acc_pair}!")
+        print(f"FOLD.{fold_id + 1} => FIN.")
 
     for summary in loss_acc_summaries:
         print(summary)
